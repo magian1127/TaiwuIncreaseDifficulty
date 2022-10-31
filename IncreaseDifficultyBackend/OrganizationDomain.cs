5f51cb9f87c663d3eb320365efd7a7bc65b332ad
Magian <zang_magian@163.com>
2022-11-01 03:09:32 +0800

    书籍保密

---
+++ IncreaseDifficultyBackend/OrganizationDomain.cs
@@ -0,0 +1,54 @@
+﻿using GameData.Common;
+using GameData.Domains;
+using GameData.Domains.Character;
+using GameData.Domains.Item;
+using GameData.Domains.Organization;
+using GameData.Utilities;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncreaseDifficultyBackend
+{
+    [HarmonyPatch]
+    public class OrganizationDomainPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(OrganizationDomain), nameof(OrganizationDomain.LeaveOrganization))]
+        public static void LeaveOrganizationPrefix(DataContext context, ref Character character, bool charIsDead)
+        {
+            OrganizationInfo orgInfo = character.GetOrganizationInfo();
+            if (orgInfo.SettlementId < 0)
+            {
+                return;
+            }
+
+            List<ItemKey> removeBooks = new List<ItemKey>();
+
+            foreach (var item in character.GetInventory().Items)
+            {
+                if (item.Key.ItemType == 10)
+                {
+                    var skillBook = Config.SkillBook.Instance[item.Key.TemplateId];
+                    if (skillBook.CombatSkillTemplateId >= 0)
+                    {
+                        var combatSkill = Config.CombatSkill.Instance[skillBook.CombatSkillTemplateId];
+                        if (combatSkill.SectId == orgInfo.OrgTemplateId && combatSkill.IsNonPublic)
+                        {
+                            removeBooks.Add(item.Key);
+                        }
+                    }
+                }
+            }
+
+            if (removeBooks.Count > 0)
+            {//离开门派时,没收所有保密书籍
+                AdaptableLog.Info($"被没收了{removeBooks.Count}本书籍");
+                character.RemoveInventoryItemList(context, removeBooks, true, false);
+            }
+        }
+    }
+}
---
+++ IncreaseDifficultyFrontend/UI_CharacterMenuItems.cs
@@ -0,0 +1,51 @@
+﻿using CharacterDataMonitor;
+using GameData.Domains.Item.Display;
+using GameData.Utilities;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+
+namespace IncreaseDifficultyFrontend
+{
+    [HarmonyPatch]
+    public class UI_CharacterMenuItemsPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(UI_CharacterMenuItems), "OnRenderItemSingle")]
+        public static void OnRenderItemSinglePrefix(UI_CharacterMenuItems __instance, ItemDisplayData itemData, ItemView itemView)
+        {
+            int taiwuId = SingletonObject.getInstance<BasicGameData>().TaiwuCharId;
+            int characterId = __instance.CharacterMenu.CurCharacterId;
+            if (characterId == taiwuId)
+            {
+                return;
+            }
+
+            if (itemData.Key.ItemType == 10)
+            {
+                var orgTemplateId = SingletonObject.getInstance<CharacterMonitorModel>().GetMonitorItem<BasicInfoMonitor>(characterId, 0, false).NameRelatedData.OrgTemplateId;
+                var skillBook = Config.SkillBook.Instance[itemData.Key.TemplateId];
+                if (skillBook.CombatSkillTemplateId >= 0)
+                {
+                    var CombatSkillItem = Config.CombatSkill.Instance[skillBook.CombatSkillTemplateId];
+                    var sectId = CombatSkillItem.SectId;
+                    if (orgTemplateId == sectId && CombatSkillItem.IsNonPublic)
+                    {
+                        itemView.SetLocked(true);
+                        int num = itemView.Names.IndexOf("Name");
+                        if (num >= 0)
+                        {
+                            itemView.CGet<TextMeshProUGUI>("Name").text = "不传之秘";
+                        }
+                        MouseTipDisplayer mouseTips = itemView.GetComponent<MouseTipDisplayer>();
+                        mouseTips.enabled = false;
+                    }
+                }
+            }
+        }
+    }
+}
